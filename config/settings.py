aac60b4b2992f72e790217ef92fad1d91f01fbfd
Oforkansi Shadrach <oforkansi.shadrach@gmail.com>
2021-04-27 05:35:53 +0000

    Deploy fixed

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,7 @@ PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT,'static'))
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT,'static'),)
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
